6d7fcae3f2aad9b3af6cf4142ca2f59ba665e65c
Gavin Hailey <gavin.hailey@ibotta.com>
2024-12-04 23:52:47 -0500

    rethink

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -3,63 +3,51 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-struct LevelValidity {
-  asc: bool,
-  desc: bool,
-  diff: bool,
-}
-
 fn main() {
     let filename = env::args().nth(1).expect("No filename provided");
     let reader = read_file(filename).expect("Could not read file");
 
-    let safe_reports_acc = reader.lines().map(|line| {
-      let vals = line.expect("Could not read line");
-
-      let mut parts = vals.split_whitespace()
-        .map(|x| x.parse::<i32>().expect("Could not parse int"))
-        .collect::<Vec<i32>>();
-
-      println!("Parts: {:?}", parts);
-      let invalid_index = check_sequence(&parts);
-      if let Some(i) = invalid_index {
-        parts.remove(i);
-        if let Some(j) = check_sequence(&parts) {
-          println!("Invalid index: {}", j);
-          return false;
-        }
-      }
-      true
+    let safe_reports_acc = reader
+      .lines()
+      .into_iter()
+      .map(|line| {
+        let seq: Vec<i32> = line
+          .expect("Could not read line")
+          .split_whitespace()
+          .map(|x| x.parse::<i32>().expect("Could not parse int"))
+          .collect();
+        is_safe_with_fault_tolerance(&seq)
     }).filter(|x| *x).count();
     println!("Safe reports: {}", safe_reports_acc);
 }
 
-fn check_sequence(seq: &Vec<i32>) -> Vec<usize> {
-  let mut invalid_indices = None;
-  let (mut only_asc, mut only_desc) = (true, true);
+fn is_safe_with_fault_tolerance(seq: &Vec<i32>) -> bool {
+  if is_safe(seq) {
+    return true;
+  }
   for i in 0..seq.len() {
-    let j = i + 1;
-    if j >= seq.len() {
-      break;
-    }
-    let qual = validate_levels(seq[i], seq[j]);
-    only_asc &= qual.asc;
-    only_desc &= qual.desc;
-    if (!only_asc && !only_desc) || qual.diff {
-      invalid_index = Some(i);
-      break;
+    let mut seq_copy = seq.clone();
+    seq_copy.remove(i);
+    if is_safe(&seq_copy) {
+      return true;
     }
   }
-  invalid_index
+  false
 }
 
-fn validate_levels(a: i32, b: i32) -> LevelValidity {
-  let diff = (a - b).abs();
-  LevelValidity {
-    asc: a < b,
-    desc: a > b,
-    diff: diff < 1 || diff > 3,
+fn is_safe(seq: &Vec<i32>) -> bool {
+  if seq.len() < 2 {
+    return false;
   }
+  let is_first_asc = seq[0] < seq[1];
+  for i in 0..(seq.len()-1) {
+    let diff = (seq[i] - seq[i+1]).abs();
+    let is_asc = seq[i] < seq[i+1];
+    if diff < 1 || diff > 3 || is_asc != is_first_asc {
+      return false;
+    }
+  }
+  true
 }
 
 fn read_file<P>(filename: P) -> io::Result<io::BufReader<File>> where P: AsRef<Path>, {
